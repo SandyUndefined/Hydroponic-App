cca5e35d69df5bf19de70ae5fdd851852ea25ddb
Sandy_undefined <08sandysk@gmail.com>
2024-07-25 11:55:46 +0530

    Corrected time k5:30 GMT

--- lib/services/thingspeak_service.dart
+++ lib/services/thingspeak_service.dart
@@ -12,41 +12,28 @@ class ThingSpeakService {
       final data = jsonDecode(response.body);
       final feeds = data['feeds'][0];
       return {
-        'Temperature': {
-          'value': feeds['field1'],
-          'time': feeds['created_at'],
-        },
-        'Humidity': {
-          'value': feeds['field2'],
-          'time': feeds['created_at'],
-        },
-        'Light Intensity': {
-          'value': feeds['field3'],
-          'time': feeds['created_at'],
-        },
-        'CO2 Concentration': {
-          'value': feeds['field4'],
-          'time': feeds['created_at'],
-        },
-        'Water Temperature': {
-          'value': feeds['field5'],
-          'time': feeds['created_at'],
-        },
-        'TDS': {
-          'value': feeds['field6'],
-          'time': feeds['created_at'],
-        },
-        'EC': {
-          'value': feeds['field7'],
-          'time': feeds['created_at'],
-        },
-        'Water Level': {
-          'value': feeds['field8'],
-          'time': feeds['created_at'],
-        },
+        'Temperature': _processField(feeds['field1'], feeds['created_at']),
+        'Humidity': _processField(feeds['field2'], feeds['created_at']),
+        'Light Intensity': _processField(feeds['field3'], feeds['created_at']),
+        'CO2 Concentration':
+            _processField(feeds['field4'], feeds['created_at']),
+        'Water Temperature':
+            _processField(feeds['field5'], feeds['created_at']),
+        'TDS': _processField(feeds['field6'], feeds['created_at']),
+        'EC': _processField(feeds['field7'], feeds['created_at']),
+        'Water Level': _processField(feeds['field8'], feeds['created_at']),
       };
     } else {
       throw Exception('Failed to load data');
     }
+  }
+
+  Map<String, String> _processField(String value, String timestamp) {
+    final dateTime = DateTime.parse(timestamp)
+        .toUtc()
+        .add(const Duration(hours: 5, minutes: 30));
+    final formattedTime =
+        "${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')} AM Today";
+    return {'value': value, 'time': formattedTime};
   }
 }
