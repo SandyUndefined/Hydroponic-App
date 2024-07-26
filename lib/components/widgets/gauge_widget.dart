bf2672ff5720bf26c8818e321f2070ac2f3ee47e
Sandy_undefined <08sandysk@gmail.com>
2024-07-26 11:43:31 +0530

    Decreased size of gauge

--- lib/components/widgets/gauge_widget.dart
+++ lib/components/widgets/gauge_widget.dart
@@ -13,44 +13,41 @@ class GaugeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SfRadialGauge(
-      title: GaugeTitle(
-        text: title,
-        textStyle: const TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      axes: <RadialAxis>[
-        RadialAxis(
-          minimum: 0,
-          maximum: 40,
-          ranges: <GaugeRange>[
-            GaugeRange(startValue: 0, endValue: 15, color: Colors.red),
-            GaugeRange(startValue: 15, endValue: 25, color: Colors.green),
-            GaugeRange(startValue: 25, endValue: 30, color: Colors.yellow),
-            GaugeRange(startValue: 30, endValue: 40, color: Colors.red),
-          ],
-          pointers: <GaugePointer>[
-            NeedlePointer(value: value),
-          ],
-          annotations: <GaugeAnnotation>[
-            GaugeAnnotation(
-              widget: Container(
-                child: Text(
-                  value.toStringAsFixed(1),
-                  style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+    return Container(
+      width: 200, // Set the desired width
+      height: 200, // Set the desired height
+      child: SfRadialGauge(
+        axes: <RadialAxis>[
+          RadialAxis(
+            minimum: 0,
+            maximum: 40,
+            ranges: <GaugeRange>[
+              GaugeRange(startValue: 0, endValue: 15, color: Colors.red),
+              GaugeRange(startValue: 15, endValue: 25, color: Colors.green),
+              GaugeRange(startValue: 25, endValue: 30, color: Colors.yellow),
+              GaugeRange(startValue: 30, endValue: 40, color: Colors.red),
+            ],
+            pointers: <GaugePointer>[
+              NeedlePointer(value: value),
+            ],
+            annotations: <GaugeAnnotation>[
+              GaugeAnnotation(
+                widget: Container(
+                  child: Text(
+                    value.toStringAsFixed(1),
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
+                angle: 90,
+                positionFactor: 0.5,
               ),
-              angle: 90,
-              positionFactor: 0.5,
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
